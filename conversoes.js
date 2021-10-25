ab0a3b0cd20377d83e53c6bc6c71b0a6cd68fe4d
Mariana Campos <marianacristinadecampos@gmail.com>
2021-10-25 09:44:20 -0300

    aulaII - declarando variaveis

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// === CONVERSOES DE TIPO === //
+
+// conversao implicita //
+
+const numero = 456;
+const numeroString = Number("456a");
+
+// Number()
+// String()
+
+console.log(numero + numeroString);
+
+// conversao explicita //
+
+console.warn(new Error('oi'));
