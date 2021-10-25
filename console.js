4f86be478ee5e4d757b6a28f0cd2fe41dbd6222d
Mariana Campos <marianacristinadecampos@gmail.com>
2021-10-25 09:47:56 -0300

    aulaIII - javascript e o nodejs

---
+++ console.js
@@ -0,0 +1,22 @@
+// === C O N S O L E === //
+
+// log == registro
+
+console.time();
+
+const minhaVar = true;
+
+// console.log(245);
+// console.log("eu sou um texto");
+// console.log(minhaVar);
+
+// tratamento de erro //
+
+console.error('deu erro rapa!');
+console.error(new Error('ih deu erro rapa!'));
+
+// console.trace();
+// trace - exibe a stacktrace de todos os pontos (ou seja, os arquivos chamados) 
+// por onde o codigo executado passou durante a execução
+
+console.timeEnd();
